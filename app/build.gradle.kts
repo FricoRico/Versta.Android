0e379ed3c98087fb91e7b4803901c65e8e36e256
Ricardo Snoek <ricardo.snoek@energyzero.nl>
2025-03-09 21:18:22 +0100

    Bumping minor version for latest bugfixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "app.versta.translate"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
